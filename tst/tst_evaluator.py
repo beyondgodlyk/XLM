065f5079f358bec61aa235530dc90121ccf7cb6e
Rohan Nayak <rohan.1998.nayak@gmail.come>
2024-07-07 18:17:46 +0200

    More changes

--- tst/tst_evaluator.py
+++ tst/tst_evaluator.py
@@ -74,7 +74,7 @@ class TSTEvaluator(Evaluator):
                 agg_label = torch.cat((agg_label, torch.Tensor([label]).repeat(pred.size()).cuda()))
 
         logger.info("Aggregate predictions shape: {}".format(agg_pred.size()))
-        assert len(agg_pred) == (4000 if data_set == 'valid' else 1000)
+        assert agg_pred.size(0) == (4000 if data_set == 'valid' else 1000)
         
         # compute accuracy, precision, recall, f1
         scores['ACC-%s' % data_set] = binary_accuracy(agg_pred, agg_label).item()
