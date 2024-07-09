eefcb7630f1edf557ee82ff0cb6eccdf1a2e6cd7
Rohan Nayak <rohan.1998.nayak@gmail.come>
2024-07-09 17:40:27 +0200

    Eval file

---
+++ style-transfer-eval.py
@@ -0,0 +1,279 @@
+import json
+import os
+import argparse
+import torch
+import random
+import torch.nn.functional as F
+from torch.nn.utils import clip_grad_norm_
+
+from xlm.utils import AttrDict
+from xlm.utils import to_cuda
+from xlm.utils import bool_flag, initialize_exp
+from xlm.data.loader import load_binarized, set_dico_parameters
+from xlm.data.dataset import ParallelDataset
+from xlm.data.dictionary import Dictionary
+from xlm.model.transformer import TransformerModel
+from xlm.optim import get_optimizer
+from xlm.evaluation.evaluator import convert_to_text
+
+from tst.tst_dataset import TSTDataset
+from tst.tst_trainer import TSTTrainer
+from tst.tst_evaluator import TSTEvaluator
+from tst.classifier import Classifier
+
+def get_parser():
+    """
+    Generate a parameters parser.
+    """
+    # parse parameters
+    parser = argparse.ArgumentParser(description="Use Classifier for TST")
+
+    # main parameters
+    parser.add_argument("--dump_path", type=str, default="./dumped/", help="Experiment dump path")
+    parser.add_argument("--exp_name", type=str, default="", help="Experiment name")
+    parser.add_argument("--exp_id", type=str, default="", help="Experiment ID")
+    parser.add_argument("--save_periodic", type=int, default=0,
+                        help="Save the model periodically (0 to disable)")
+    # batch parameters
+    parser.add_argument("--batch_size", type=int, default=32, help="Number of sentences per batch")
+    parser.add_argument("--max_len", type=int, default=30,
+                        help="Maximum length of sentences (after BPE)")
+    parser.add_argument("--tokens_per_batch", type=int, default=-1,
+                        help="Number of tokens per batch")
+    parser.add_argument("--max_batch_size", type=int, default=0,
+                        help="Maximum number of sentences per batch (used in combination with tokens_per_batch, 0 to disable)")
+    
+    # model parameters
+    parser.add_argument("--gelu_activation", type=bool_flag, default=True,
+                        help="Use a GELU activation instead of ReLU")
+    
+    # model / output paths
+    parser.add_argument("--dae_model_path", type=str, default="", help="DAE Model path")
+    parser.add_argument("--classifier_model_path", type=str, default="", help="Classifier Model path")
+    parser.add_argument("--output_path", type=str, default="", help="Output path")
+
+    # reload checkpoint
+    parser.add_argument("--reload_checkpoint", type=str, default="",
+                        help="Reload a checkpoint")
+
+    parser.add_argument("--max_vocab", type=int, default=-1, help="Maximum vocabulary size (-1 to disable)")
+    parser.add_argument("--min_count", type=int, default=0, help="Minimum vocabulary count")
+
+    # data
+    parser.add_argument("--data_path", type=str, default="",
+                        help="Data path")
+    parser.add_argument("--src_lang", type=str, default="", help="Source language")
+    parser.add_argument("--tgt_lang", type=str, default="", help="Target language")
+
+    # training parameters
+    parser.add_argument("--optimizer", type=str, default="adam,lr=0.0001",
+                        help="Optimizer (SGD / RMSprop / Adam, etc.)")
+    parser.add_argument("--clip_grad_norm", type=float, default=5,
+                        help="Clip gradients norm (0 to disable)")
+        
+    # float16 / AMP API
+    parser.add_argument("--amp", type=int, default=-1,
+                        help="Use AMP wrapper for float16 / distributed / gradient accumulation. Level of optimization. -1 to disable.")
+    
+    # memory parameters
+    parser.add_argument("--use_memory", type=bool_flag, default=False,
+                        help="Use an external memory")
+    
+    # debug (required for using load binarized)
+    parser.add_argument("--debug_train", type=bool_flag, default=False,
+                        help="Use valid sets for train sets (faster loading)")
+    
+    # Datasets for EN
+    parser.add_argument("--use_yelp_EN_lowercase", type=bool_flag, default=True,
+                        help="True if Yelp EN dataset with lowercased text is used for domain adaptive training")
+    
+    return parser
+
+def check_params(params):
+    """
+    Check parameters and add necessary ones.
+    """
+    # Disabled Multi GPU for the training to finish
+    params.multi_gpu = False
+
+    # data path
+    assert os.path.isdir(params.data_path), params.data_path
+
+    # check languages
+    params.langs = ['en']
+    # assert sorted(params.langs) == params.langs
+    params.id2lang = {k: v for k, v in enumerate(sorted(params.langs))}
+    params.lang2id = {k: v for v, k in params.id2lang.items()}
+    params.n_langs = len(params.langs)
+
+    # Parse kernel sizes
+    assert params.kernel_sizes != ''
+    params.kernel_sizes = list(map(int, params.kernel_sizes.split(',')))
+
+    # Parse fc sizes
+    params.fc_sizes = list(map(int, params.fc_sizes.split(','))) if params.fc_sizes != '' else []
+    assert len(params.fc_sizes) >= 2
+    assert params.fc_sizes[-1] == 1
+    assert params.fc_sizes[0] == params.num_filters * len(params.kernel_sizes)
+
+    # check datasets
+    required_tst = set(params.langs)
+    params.tst_test_dataset = {
+        lang: {
+            label_pairs: (os.path.join(params.data_path, 'tst.%s.%s.%s.pth' % (lang, label_pairs, label_pairs[0])),
+                   os.path.join(params.data_path, 'tst.%s.%s.%s.pth' % (lang, label_pairs, label_pairs[1])))
+            for label_pairs in [(0, 1), (1, 0)]
+        } for lang in params.langs if lang in required_tst
+    }
+    for paths in params.tst_test_dataset.values():
+        for p1, p2 in paths.values():
+            assert os.path.isfile(p1), "%s not found" % p1
+            assert os.path.isfile(p2), "%s not found" % p2
+
+def load_tst_test_data(params, logger):
+    data = {}
+    data['tst'] = {}
+
+    for lang in params.langs:
+        data['tst'][lang] = {}
+        for label_pair in [(0, 1), (1, 0)]: # (0, 1) for positive to negative, (1, 0) for negative to positive
+            data['tst'][lang][label_pair] = {}
+            src_style_data_path, tgt_style_data_path = params.tst_test_dataset[lang][label_pair]
+
+            src_style_data = load_binarized(src_style_data_path, params)
+            tgt_style_data = load_binarized(tgt_style_data_path, params)
+
+            set_dico_parameters(params, data, src_style_data['dico'])
+            set_dico_parameters(params, data, tgt_style_data['dico'])
+
+            data['tst'][lang][label_pair] = (TSTDataset(src_style_data['sentences'], src_style_data['positions'], params, label_pair[0]),
+                                             TSTDataset(tgt_style_data['sentences'], tgt_style_data['positions'], params, label_pair[1]))
+
+    # TST test data summary
+    logger.info('============ Data summary')
+    for label_pair, v in data['tst'].items():
+        for data_set in v.keys():
+            logger.info('{: <18} - {: >5} - {: >12}:{: >10}'.format('TST test data', data_set, '%s-%s' % label_pair, len(v[data_set][0])))
+
+    return data
+
+def reload_models(params):
+    reloaded_dae = torch.load(params.dae_model_path)
+    dae_model_params = AttrDict(reloaded_dae['params'])
+
+    # update dictionary parameters
+    for name in ['n_words', 'bos_index', 'eos_index', 'pad_index', 'unk_index', 'mask_index']:
+        setattr(params, name, getattr(dae_model_params, name))
+
+    # build dictionary / build encoder / build decoder / reload weights
+    dico = Dictionary(reloaded_dae['dico_id2word'], reloaded_dae['dico_word2id'], reloaded_dae['dico_counts'])
+    encoder = TransformerModel(dae_model_params, dico, is_encoder=True, with_output=True).cuda().eval()
+    decoder = TransformerModel(dae_model_params, dico, is_encoder=False, with_output=True).cuda().eval()
+    encoder.load_state_dict(reloaded_dae['encoder'])
+    decoder.load_state_dict(reloaded_dae['decoder'])
+    params.src_id = dae_model_params.lang2id[params.src_lang]
+    params.tgt_id = dae_model_params.lang2id[params.tgt_lang]
+
+    # build classifier
+    reloaded_classifier = torch.load(params.classifier_model_path)
+    classifier_model_params = AttrDict(reloaded_classifier['params'])
+    classifier = Classifier(dae_model_params.emb_dim, 
+                            classifier_model_params.kernel_sizes, 
+                            classifier_model_params.dropout, 
+                            classifier_model_params.fc_sizes, 
+                            classifier_model_params.num_filters, 
+                            classifier_model_params.max_len).cuda().eval()
+    classifier.load_state_dict(reloaded_classifier['classifier'])
+
+    return dico, encoder, decoder, classifier
+
+def get_transferred_sentence(len1, lang2_id, modified_enc, decoder, dico):
+    max_len = int(1.5 * len1.max().item() + 10)
+    generated, lengths = decoder.generate(modified_enc, len1, lang2_id, max_len=max_len)
+    return convert_to_text(generated, lengths, dico, params)
+
+def main(params):
+
+    # initialize the experiment
+    logger = initialize_exp(params)
+    
+    if not os.path.isfile(params.output_path):
+        params.output_path = os.path.join(params.dump_path, "%s-%s.txt" % (params.src_lang, params.tgt_lang))
+
+    data = load_tst_test_data(params, logger)
+
+    dico, encoder, decoder, classifier = reload_models(params)
+
+    assert params.batch_size == 1
+
+    for lang in params.langs:
+        for label_pair in [(0, 1), (1, 0)]:
+            src_iterator = data['tst'][lang][label_pair][0].get_iterator(shuffle=False, 
+                                                                        group_by_size=False, 
+                                                                        n_sentences=-1)
+            tgt_iterator = data['tst'][lang][label_pair][1].get_iterator(shuffle=False,
+                                                                        group_by_size=False,
+                                                                        n_sentences=-1)
+            
+            for src_batch, tgt_batch in zip(src_iterator, tgt_iterator):
+                (x1, len1) = src_batch
+                (x2, len2) = tgt_batch
+
+                langs1 = x1.clone().fill_(params.src_id)
+                langs2 = x2.clone().fill_(params.tgt_id)
+
+                x1, len1, langs1, x2, len2, langs2 = to_cuda(x1, len1, langs1, x2, len2, langs2)
+
+                enc1 = encoder('fwd', x=x1, lengths=len1, langs=langs1, causal=False)
+                enc1 = enc1.transpose(0, 1)
+
+                # Clone detached encoder output to be modified iteratively
+                modified_enc1 = enc1.detach().clone()
+                modified_enc1.requires_grad = True
+
+                logger.info("Original sentence: %s" % get_transferred_sentence(len1, params.tgt_id, enc1, decoder, dico))
+                logger.info("Gold sentence: %s" % convert_to_text(x2, len2, dico, params))
+
+                opt = get_optimizer([modified_enc1], params.optimizer)
+                it = 0
+                while True:
+                    pred = classifier(modified_enc1).squeeze(1)
+                    loss = F.binary_cross_entropy(pred, torch.Tensor([label_pair[1]]).repeat(pred.size()).cuda(), reduction='none')
+                    logger.info("Iteration %d, Pred: %f, Loss: %f" % (it, pred, loss.item()))
+                    logger.info("Modified sentence: %s" % get_transferred_sentence(len1, params.tgt_id, modified_enc1, decoder, dico))
+                
+                    if loss.item() < 0.1:
+                        break
+
+                    opt.zero_grad()
+
+                    for i in range(len(loss)):
+                        loss[i].backward()
+
+                    clip_grad_norm_([modified_enc1], params.clip_grad_norm)
+                    opt.step()
+                    it += 1
+                    assert torch.all(enc1 == modified_enc1) == False, "Modified encoder output is same as original encoder output"
+
+
+                    
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    # generate parser / parse parameters
+    parser = get_parser()
+    params = parser.parse_args()
+
+    # check parameters
+    assert os.path.isfile(params.dae_model_path)
+    assert os.path.isfile(params.classifier_model_path)
+    assert params.src_lang != '' and params.tgt_lang != ''
+    # assert params.output_path and not os.path.isfile(params.output_path)
+
+    check_params(params)
+    main(params)
