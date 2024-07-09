45621a9a88270fd1a462777375f1760baafa3f94
Rohan Nayak <rohan.1998.nayak@gmail.come>
2024-07-09 16:57:00 +0200

    A bug

--- style-transfer-train.py
+++ style-transfer-train.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
     params = parser.parse_args()
 
     # check parameters
-    assert os.path.isfile(params.model_path)
+    assert os.path.isfile(params.dae_model_path)
     assert params.src_lang != '' and params.tgt_lang != '' # and params.src_lang != params.tgt_lang
     # assert params.output_path and not os.path.isfile(params.output_path)
 
