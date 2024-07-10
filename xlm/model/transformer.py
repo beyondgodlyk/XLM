d3a634781db00179c4da6e7ba2f19e0d43aec6cf
Rohan Nayak <rohan.1998.nayak@gmail.come>
2024-07-10 11:41:40 +0200

    Trying bug fix

--- xlm/model/transformer.py
+++ xlm/model/transformer.py
@@ -209,6 +209,9 @@ class MultiHeadAttention(nn.Module):
         q = q / math.sqrt(dim_per_head)                                       # (bs, n_heads, qlen, dim_per_head)
         scores = torch.matmul(q, k.transpose(2, 3))                           # (bs, n_heads, qlen, klen)
         if mask.dim() == 3:
+            z = mask.clone()
+            t = (z == 0).view(mask_reshape)
+            u = t.expand_as(scores)
             logger.info('In MultiHeadAttention.forward()')
             logger.info('mask shape: %s', mask.shape)
             logger.info('scores shape: %s', scores.shape)
