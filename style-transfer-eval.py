f3b840c35d939f71164ed442f83e2c4c58a6a8bd
Rohan Nayak <rohan.1998.nayak@gmail.come>
2024-07-12 17:38:55 +0200

    Checking distance

--- style-transfer-eval.py
+++ style-transfer-eval.py
@@ -264,7 +264,7 @@ def main(params):
                         logger.info("Modified encoder output has not changed. Continuing")
                         break
                     it += 1
-                    if it >= 50:
+                    if it >= 200:
                         break
                 # TODO : restore segmentation
 
