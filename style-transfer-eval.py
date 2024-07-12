383a30c5db4567b8afc4d774062d1b8b6b81844a
Rohan Nayak <rohan.1998.nayak@gmail.come>
2024-07-12 17:34:32 +0200

    Checking distance

--- style-transfer-eval.py
+++ style-transfer-eval.py
@@ -253,10 +253,9 @@ def main(params):
                     if params.clip_grad_norm > 0:
                         clip_grad_norm_([modified_enc1], params.clip_grad_norm)
                     opt.step()
-                    logger.info(LA.matrix_norm(modified_enc1.grad.data))
                     logger.info("Iteration %d, Pred: %.10f, Loss: %.20f, Gradient Norm: %.20f, Max: %.5f, Min: %.5f" % 
-                                (it, pred[0], loss[0].item(), LA.matrix_norm(modified_enc1.grad.data).item(), 
-                                 LA.matrix_norm(modified_enc1, ord=2).item(), LA.matrix_norm(modified_enc1, ord=-2).item()))
+                                (it, pred[0], loss[0].item(), LA.matrix_norm(modified_enc1.grad.data)[0].item(), 
+                                 LA.matrix_norm(modified_enc1, ord=2)[0].item(), LA.matrix_norm(modified_enc1, ord=-2)[0].item()))
                     logger.info("Modified sentence: %s" % 
                                 get_transferred_sentence(len1, params.tgt_id, modified_enc1, decoder, dico, params)[0])
                     logger.info("")
