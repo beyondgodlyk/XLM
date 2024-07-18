2e8b0e6c2d19afc45e3b75a8716d2fdf098f6028
Rohan Nayak <rohan.1998.nayak@gmail.come>
2024-07-18 23:53:10 +0200

    Sanity checks related changes

--- tst/tst_trainer.py
+++ tst/tst_trainer.py
@@ -135,10 +135,10 @@ class TSTTrainer(Trainer):
         # Make sure the params of classifier are correctly loaded in the optimizer
         assert self.parameters[cl_opt_keys[0]] == [p for p in list(self.classifier.parameters()) if p.requires_grad]
         # Make sure all the params of Classifier are being updated
-        print([p.grad != None for p in list(self.classifier.parameters()) if p.requires_grad])
         assert sum([p.grad != None for p in list(self.classifier.parameters()) if p.requires_grad]) == len(self.parameters[cl_opt_keys[0]])
         
         # Since LHS contains params for Enc+Dec, make sure that the length of params which have grad (only Enc) are less
+        print([p.grad != None for p in list(self.encoder.parameters()) if p.requires_grad])
         assert len(self.parameters[dae_opt_keys[0]]) > sum([p.grad != None for p in list(self.encoder.parameters()) if p.requires_grad])
         # Make sure all the params of Enc are being updated
         assert len([p for p in list(self.encoder.parameters()) if p.requires_grad]) == sum([p.grad != None for p in list(self.encoder.parameters()) if p.requires_grad]) 
