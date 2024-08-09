d4e40ca22e93e488fe73173afd8a6881209ca703
Rohan Nayak <rohan.1998.nayak@gmail.come>
2024-08-09 23:17:00 +0200

    Changes for XLM classifier
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier
    
    Changes for XLM classifier

--- xlm_roberta_classifier.py
+++ xlm_roberta_classifier.py
@@ -7,7 +7,7 @@ import os
 import argparse
 import torch
 import random
-
+import torch.nn as nn
 
 from xlm.utils import to_cuda
 
@@ -15,6 +15,20 @@ from xlm.utils import bool_flag, initialize_exp
 
 from torch.utils.data import Dataset
 from torchtext.models import RobertaClassificationHead, XLMR_BASE_ENCODER
+from torch.utils.data import DataLoader
+import torchtext.functional as F
+from torch.optim import AdamW
+
+class SentenceDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
 
 def get_parser():
     """
@@ -27,75 +41,28 @@ def get_parser():
     parser.add_argument("--dump_path", type=str, default="./dumped/", help="Experiment dump path")
     parser.add_argument("--exp_name", type=str, default="", help="Experiment name")
     parser.add_argument("--exp_id", type=str, default="", help="Experiment ID")
-    parser.add_argument("--save_periodic", type=int, default=0,
-                        help="Save the model periodically (0 to disable)")
+
     # batch parameters
     parser.add_argument("--batch_size", type=int, default=32, help="Number of sentences per batch")
-    parser.add_argument("--max_len", type=int, default=30,
-                        help="Maximum length of sentences (after BPE)")
-    parser.add_argument("--tokens_per_batch", type=int, default=-1,
-                        help="Number of tokens per batch")
-    parser.add_argument("--max_batch_size", type=int, default=0,
-                        help="Maximum number of sentences per batch (used in combination with tokens_per_batch, 0 to disable)")
-    
-    # model parameters
-    parser.add_argument("--gelu_activation", type=bool_flag, default=True,
-                        help="Use a GELU activation instead of ReLU")
-    
-    # output paths
-    parser.add_argument("--output_path", type=str, default="", help="Output path")
-
-    # reload checkpoint
-    parser.add_argument("--reload_checkpoint", type=str, default="",
-                        help="Reload a checkpoint")
-
-    parser.add_argument("--max_vocab", type=int, default=-1, help="Maximum vocabulary size (-1 to disable)")
-    parser.add_argument("--min_count", type=int, default=0, help="Minimum vocabulary count")
 
     # data
     parser.add_argument("--data_path", type=str, default="",
                         help="Data path")
-    parser.add_argument("--src_lang", type=str, default="", help="Source language")
-    parser.add_argument("--tgt_lang", type=str, default="", help="Target language")
-
-    # input sentence noise
-    parser.add_argument("--word_shuffle", type=float, default=0,
-                        help="Randomly shuffle input words (0 to disable)")
-    parser.add_argument("--word_dropout", type=float, default=0,
-                        help="Randomly dropout input words (0 to disable)")
-    parser.add_argument("--word_blank", type=float, default=0,
-                        help="Randomly blank input words (0 to disable)")
-
-    # training parameters
-    parser.add_argument("--optimizer", type=str, default="adam,lr=0.0001",
-                        help="Optimizer (SGD / RMSprop / Adam, etc.)")
-    parser.add_argument("--clip_grad_norm", type=float, default=5,
-                        help="Clip gradients norm (0 to disable)")
-    parser.add_argument("--epoch_size", type=int, default=100000,
-                        help="Epoch size / evaluation frequency (-1 for parallel data size)")
-    parser.add_argument("--max_epoch", type=int, default=100000,
-                        help="Maximum epoch size")
-    parser.add_argument("--stopping_criterion", type=str, default="",
-                        help="Stopping criterion, and number of non-increase before stopping the experiment")
-    parser.add_argument("--validation_metrics", type=str, default="",
-                        help="Validation metrics")
-        
-    # float16 / AMP API
-    parser.add_argument("--amp", type=int, default=-1,
-                        help="Use AMP wrapper for float16 / distributed / gradient accumulation. Level of optimization. -1 to disable.")
-    
-    # memory parameters
-    parser.add_argument("--use_memory", type=bool_flag, default=False,
-                        help="Use an external memory")
-    
-    # debug (required for using load binarized)
-    parser.add_argument("--debug_train", type=bool_flag, default=False,
-                        help="Use valid sets for train sets (faster loading)")
     
     # Datasets for EN
     parser.add_argument("--use_yelp_EN_lowercase", type=bool_flag, default=True,
                         help="True if Yelp EN dataset with lowercased text is used for domain adaptive training")
     
+    parser.add_argument("--train", type=bool_flag, default=False,
+                        help="True if XLM Roberta classifier is to be trained")
+    parser.add_argument("--eval", type=bool_flag, default=True,
+                        help="True if XLM Roberta classifier is to be evaluated")
+    parser.add_argument("--eval_model_path", type=str, default="dumped/xlm_roberta/w1dn1d4ku7/best_model.pt",
+                        help="Path to the model to be used for evaluation")
+    parser.add_argument("--eval_file_path", type=str, default="",
+                        help="Path to the file to be evaluated")
+    parser.add_argument("--print_mismatches", type=bool_flag, default=False,
+                        help="True if mismatches are to be printed")
     return parser
 
 def check_params(params):
@@ -117,14 +84,14 @@ def check_params(params):
 
     params.labels = [0, 1]
 
-    params.tst_train_dataset = {
+    params.xlm_classifier_train_dataset = {
         lang: {
             splt: (os.path.join(params.data_path, 'tst.%s.0.%s.tok' % (splt, lang)),
                    os.path.join(params.data_path, 'tst.%s.1.%s.tok' % (splt, lang)))
             for splt in ['train', 'valid', 'test']
         } for lang in params.langs
     }
-    for paths in params.tst_train_dataset.values():
+    for paths in params.xlm_classifier_train_dataset.values():
         for p1, p2 in paths.values():
             assert os.path.isfile(p1), "%s not found" % p1
             assert os.path.isfile(p2), "%s not found" % p2
@@ -139,75 +106,126 @@ def load_tokenized_data(path, params, text_transform):
 
 def load_tst_train_data(params, logger, text_transform):
     data = {}
-    data['tst'] = {}
-
+    data['xlm_classifier'] = {}
+    
     for lang in params.langs:
-        for label in params.labels:
-            data['tst'][label] = {}
-            for splt in ['train', 'valid', 'test']:
-                style_data = load_tokenized_data(params.tst_train_dataset[lang][splt][label], params)
-                data['tst'][label][splt] = SentenceDataset(style_data, [label] * len(style_data))
+        for splt in ['train', 'valid', 'test']:
+            style_data = []
+            labels = []
+            for label in params.labels:
+                style_data += load_tokenized_data(params.xlm_classifier_train_dataset[lang][splt][label], params, text_transform)
+                labels += [label] * len(style_data)
+            data['xlm_classifier'][splt] = SentenceDataset(style_data, labels)
 
     # TST train data summary
     logger.info('============ Data summary')
-    for label, v in data['tst'].items():
-        for data_set in v.keys():
-            logger.info('{: <18} - {: >5} - {: >12}:{: >10}'.format('TST train data', data_set, label, len(v[data_set])))
+    for data_set, v in data['xlm_classifier'].items():
+        logger.info('{: <18} - {: >5} - {: >10}'.format('XLM Classifier train data', data_set, len(v)))
 
     return data
 
+def collate_fn(batch):
+    """
+    Collate function to be used when creating a DataLoader.
+    """
+    data = [item[0] for item in batch] # Size B X S
+    labels = [item[1] for item in batch]
+    return data, labels
+
 def main(params):
 
-    # initialize the experiment
-    logger = initialize_exp(params)
-    
-    if not os.path.isfile(params.output_path):
-        params.output_path = os.path.join(params.dump_path, "%s-%s.txt" % (params.src_lang, params.tgt_lang))
+    padding_idx = 1
+    bos_idx = 0
+    eos_idx = 2
+    DEVICE = 'cuda'
 
-    # num_classes = 2
-    # input_dim = 768
-    # classifier_head = RobertaClassificationHead(num_classes=num_classes, input_dim=input_dim)
-    # xlm_classifier = XLMR_BASE_ENCODER.get_model(head=classifier_head)
-    # xlm_classifier.to('cuda')
+    num_classes = 2
+    input_dim = 768
+    classifier_head = RobertaClassificationHead(num_classes=num_classes, input_dim=input_dim)
+    xlm_classifier = XLMR_BASE_ENCODER.get_model(head=classifier_head)
+    xlm_classifier.to('cuda')
 
     text_transform = XLMR_BASE_ENCODER.transform()
 
+    if params.eval:
+        xlm_classifier.load_state_dict(torch.load(params.eval_model_path))
+        with open(params.eval_file_path, "r", encoding='utf-8') as f:
+            reviews = [text_transform(line.rstrip()) for line in f]
+        true_labels = [1] * (len(reviews)//2) + [0] * (len(reviews)//2)
+        with torch.no_grad():
+            xlm_classifier.eval()
+            test_loader = DataLoader(SentenceDataset(reviews, true_labels), batch_size = params.batch_size, shuffle=False, collate_fn=collate_fn)
+            total = 0
+            correct = 0
+            pred = []
+            for batch in test_loader:
+                input = F.to_tensor(batch[0], padding_value=padding_idx).to(DEVICE)
+                output = xlm_classifier(input)
+                target = torch.tensor(batch[1], dtype=torch.long).to(DEVICE)
+                _, predicted = torch.max(output, 1)
+                pred += predicted.tolist()
+                total += len(batch[1])
+                correct += (predicted == target).sum().item()
+            accuracy = correct / total
+            if params.print_mismatches:
+                for i in range(len(true_labels)):
+                    if true_labels[i] != pred[i]:
+                        print(f"{i}: {true_labels[i]} - {pred[i]}")
+            print(f'Accuracy: {accuracy}')
+
+        return
+
+    # initialize the experiment
+    logger = initialize_exp(params)
+
     data = load_tst_train_data(params, logger, text_transform)
-    print(data['tst'][0]['train'][0])
+    learning_rate = 1e-5
+    optim = AdamW(xlm_classifier.parameters(), lr=learning_rate)
+    criteria = nn.CrossEntropyLoss()
 
+    score_tracker = []
+    epoch_for_best_score = -1
+    for epoch in range(params.max_epoch):
+        xlm_classifier.train()
+        train_loader = DataLoader(data['xlm_classifier']['train'], batch_size = params.batch_size, shuffle=True, collate_fn=collate_fn)
+        sent = 0
+        for batch in train_loader:
+            input = F.to_tensor(batch[0], padding_value=padding_idx).to(DEVICE)
+            output = xlm_classifier(input)
+            target = torch.tensor(batch[1], dtype=torch.long).to(DEVICE)
+            loss = criteria(output, target)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            sent += len(batch[1])
+            if sent % 5*params.batch_size == 0:
+                logger.info(f'Epoch {epoch} - Sentences: {sent} - Loss: {loss.item()}')
 
-
-    # trainer = XLMClassifierTrainer(xlm_classifier, data, params)
-    # evaluator = TSTEvaluator(trainer, data, params)
-
-    # for _ in range(params.max_epoch):
-
-    #     logger.info("============ Starting epoch %i ... ============" % trainer.epoch)
-
-    #     trainer.n_sentences = 0
-
-    #     while trainer.n_sentences < trainer.epoch_size:
-    #         for label in random.sample(params.labels, len(params.labels)):
-    #             trainer.classifier_step(label)
-
-    #         trainer.iter()
-        
-    #     logger.info("============ End of epoch %i ============" % trainer.epoch)
-        
-    #     # Evaluate on classification metrics
-    #     scores = evaluator.run_all_evals(trainer)
-
-    #     # print / JSON log
-    #     for k, v in scores.items():
-    #         logger.info("%s -> %.6f" % (k, v))
-    #     if params.is_master:
-    #         logger.info("__log__:%s" % json.dumps(scores))
-
-    #     # end of epoch
-    #     trainer.save_best_model(scores)
-    #     trainer.save_periodic()
-    #     trainer.end_epoch(scores)
-
+        with torch.no_grad():
+            xlm_classifier.eval()
+            valid_loader = DataLoader(data['xlm_classifier']['valid'], batch_size = 32, shuffle=False, collate_fn=collate_fn)
+            total = 0
+            correct = 0
+            total_loss = 0
+            for batch in valid_loader:
+                input = F.to_tensor(batch[0], padding_value=padding_idx).to(DEVICE)
+                output = xlm_classifier(input)
+                target = torch.tensor(batch[1], dtype=torch.long).to(DEVICE)
+                loss = criteria(output, target)
+                total_loss += loss.item()
+                _, predicted = torch.max(output, 1)
+                total += len(batch[1])
+                correct += (predicted == target).sum().item()
+            accuracy = correct / total
+            logger.info(f'Epoch {epoch} - Loss: {total_loss/total} - Accuracy: {accuracy}')
+            if epoch_for_best_score == -1 or accuracy > max(score_tracker):
+                epoch_for_best_score = epoch
+                torch.save(xlm_classifier.state_dict(), os.path.join(params.dump_path, "best_model.pt"))
+                logger.info(f"Best model saved for epoch {epoch}.")
+            score_tracker.append(accuracy)
+            if epoch - epoch_for_best_score >= 3:
+                break
+        torch.save(xlm_classifier.state_dict(), os.path.join(params.dump_path, f"model_{epoch}.pt"))
 
 if __name__ == '__main__':
 
@@ -215,21 +233,5 @@ if __name__ == '__main__':
     parser = get_parser()
     params = parser.parse_args()
 
-    # check parameters
-    assert params.src_lang != '' and params.tgt_lang != '' # and params.src_lang != params.tgt_lang
-    # assert params.output_path and not os.path.isfile(params.output_path)
-
     check_params(params)
     main(params)
-
-
-class SentenceDataset(Dataset):
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx]
